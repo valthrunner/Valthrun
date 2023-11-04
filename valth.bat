9d273edb16ce4e9e924819425fc62156af0ffe50
Valthrunner <131185036+valthrunner@users.noreply.github.com>
2023-11-04 01:22:27 +0100

    Update valth.bat

--- valth.bat
+++ valth.bat
@@ -74,9 +74,9 @@ if "!cleanCurrentVersion!" lss "!cleanLatestVersion!" (
     :: Download the new version
     :downloadController
 
-    curl -s -L -o "controller.exe" "https://github.com/Valthrun/Valthrun/releases/latest/download/controller.exe"
+    :: curl -s -L -o "controller.exe" "https://github.com/Valthrun/Valthrun/releases/latest/download/controller.exe"
     :: dont use my controller compile bc offsets changed (but still in here if needed one day)
-    :: curl -s -L -o "controller.exe" "https://github.com/valthrunner/Valthrun/releases/latest/download/controller.exe"
+    curl -s -L -o "controller.exe" "https://github.com/valthrunner/Valthrun/releases/latest/download/controller.exe"
     echo  Download complete: controller.exe
     echo.
 
