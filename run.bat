bec562b60f171ac6c30e75b0621c873f8d623483
Valthrunner <131185036+valthrunner@users.noreply.github.com>
2023-11-07 19:06:02 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,6 +9,6 @@ title %scriptTitle%
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-curl -s -L -o "%temp%/valth.bat" "https://github.com/valthrunner/Valthrun/releases/latest/download/valth.bat"
+curl -s -L -o "%temp%/valth.bat" "https://raw.githubusercontent.com/valthrunner/Valthrun/main/valth.bat"
 
 %temp%/valth.bat run
