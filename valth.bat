920e995a4e8d43f724d0c6b21a345af396b3b649
Valthrunner <131185036+valthrunner@users.noreply.github.com>
2024-02-09 02:07:30 +0100

    v3.0
    cleanup and fixes

--- valth.bat
+++ valth.bat
@@ -1,295 +1,85 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-:: Define script title
-set "scriptTitle=Valthrunner's Script v2.3"
-title %scriptTitle%
+:: Define script title and set initial variables
+set "title=Valthrunner's Script v3.0"
+title %title%
+set "mode=0"
 
-:: Check if this script was called from another script
+:: Set mode based on arguments
 if "%~1"=="run" (
     echo.
+) else if "%~1"=="run_radar" (
+    set "mode=1"
+    mode 85, 40
+    echo.
 ) else (
-    if "%~1"=="run_radar" (
-        SET /A RADAR=1
-        mode 85, 40
-        echo.
-    ) else (
-        mode 85, 30
-        echo  Please use run.bat.
-        echo  Downloading run.bat...
-        curl -s -L -o "run.bat" "https://github.com/valthrunner/Valthrun/releases/latest/download/run.bat"
-        call run.bat
-        exit
-    )
+    mode 85, 30
+    echo   Please use run.bat.
+    echo   Downloading run.bat...
+    curl -s -L -o "run.bat" "https://github.com/valthrunner/Valthrun/releases/latest/download/run.bat"
+    call run.bat
+    exit
 )
 
+:: Display ASCII art header
 echo.
-:::[1[37m  _   __     ____  __                              [31m/[37m       ____        _      __ [0m
-:::[1[93m | | / /__ _/ / /_/ /  ______ _____  ___  ___ ____  ___   / __/_______(_)__  / /_[0m
-:::[1[33m | |/ / _ `/ / __/ _ \/ __/ // / _ \/ _ \/ -_) __/ (_-<  _\ \/ __/ __/ / _ \/ __/[0m
-:::[1[31m |___/\_,_/_/\__/_//_/_/  \_,_/_//_/_//_/\__/_/   /___/ /___/\__/_/ /_/ ___/\__/ [0m
-:::[1[31m                                                                     /_/         [0m
+call :displayHeader
 
-for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+:: Fetch the newest release using PowerShell
+set "tagsUrl=https://api.github.com/repos/Valthrun/Valthrun/tags"
+for /f "delims=" %%i in ('powershell -Command "$response = Invoke-WebRequest -Uri '%tagsUrl%' -UseBasicParsing; $tags = $response.Content | ConvertFrom-Json; if ($tags.Count -gt 0) { $tags[0].name } else { 'No tags found' }"') do set "newestTag=%%i"
 
-if "%RADAR%" == "1" (
-echo  using radar version of valthrun!
-echo.
-)
 
-goto download
+:: Construct the download URLs based on the newest tag
+set "baseDownloadUrl=https://github.com/Valthrun/Valthrun/releases/download/%newestTag%/"
+set "baseRunnerDownloadUrl=https://github.com/valthrunner/Valthrun/releases/latest/download/"
 
-::this is skipped for now
-
-:: Fetch the latest release info from GitHub
-curl -s https://api.github.com/repos/Valthrun/Valthrun/releases/latest > latest.json
-
-:: Check if the files exist
-if not exist "controller.exe" (
-    echo  controller.exe does not exist. Downloading...
-    echo.
-    goto :downloadController
-) else if not exist "valthrun-driver.sys" (
-    echo  valthrun-driver.sys does not exist. Downloading...
-    echo.
-    goto :downloadDriver
-) else if not exist "kdmapper.exe" (
-    echo  kdmapper.exe does not exist. Downloading...
-    echo.
-    goto :downloadKDMapper
-)
-
-:: Get the current version of the file
-for /f "tokens=2 delims==" %%a in ('wmic datafile where "name='%%cd:\=\\%%\\controller.exe'" get version /value ^| find "Version="') do (
-    set "currentVersion=%%a"
-)
-
-:: Get the latest version number from GitHub using PowerShell and remove "v" prefix
-for /f "delims=" %%i in ('powershell -Command "(Get-Content latest.json | ConvertFrom-Json | Select-Object -ExpandProperty tag_name).Replace('v', '')"') do set "latestVersion=%%i"
-
-:: Extract version numbers for comparison
-set "cleanCurrentVersion=!currentVersion:v=!"
-set "cleanLatestVersion=!latestVersion!"
-
-:: Compare version numbers
-if "!cleanCurrentVersion!" lss "!cleanLatestVersion!" (
-    echo  New version available: v%latestVersion%
-    :download
-    echo.
-    echo  Downloading...
-    echo.
-
-    :: Download the new version
-    :downloadController
-
-    if "%RADAR%" == "1" (
-        curl -s -L -o "controller.exe" "https://github.com/valthrunner/Valthrun/releases/latest/download/controller_radar.exe"
-        echo  Download complete: controller.exe (radar version!^)
-        echo.
-        curl -s -L -o "%temp%/radar.zip" "https://github.com/valthrunner/Valthrun/releases/latest/download/web_radar_server.zip"
-        echo  Download complete: radar.zip
-        powershell.exe -Command "Expand-Archive -Path '%temp%\radar.zip' -DestinationPath './' -Force"
-        del "%temp%\radar.zip"
-        echo.   
-    ) else (
-    curl -s -L -o "controller.exe" "https://github.com/Valthrun/Valthrun/releases/latest/download/controller.exe"
-    :: curl -s -L -o "controller.exe" "https://github.com/valthrunner/Valthrun/releases/latest/download/controller.exe"
-    echo  Download complete: controller.exe
-    echo.
-    )
-
-    :downloadDriver
-    curl -s -L -o "valthrun-driver.sys" "https://github.com/Valthrun/Valthrun/releases/latest/download/valthrun-driver.sys"
-    echo  Download complete: valthrun-driver.sys
-    echo.
-
-    :downloadKDMapper
-    curl -s -L -o "kdmapper.exe" "https://github.com/valthrunner/Valthrun/releases/latest/download/kdmapper.exe"
-    echo  Download complete: kdmapper.exe
-    echo.
-    
-    goto :cleanup
-
+:: Handle radar version
+if "%mode%" == "1" (
+    goto radar
 ) else (
-    echo  No new version available.
     echo.
+    echo   Downloading necessary files...
+    call :downloadFileWithFallback "%baseDownloadUrl%controller.exe" "%baseRunnerDownloadUrl%controller.exe" "controller.exe"
+    call :downloadFile "%baseDownloadUrl%valthrun-driver.sys" "valthrun-driver.sys"
+    call :downloadFile "%baseRunnerDownloadUrl%kdmapper.exe" "kdmapper.exe"
 )
 
 :cleanup
-
-if exist "latest.json" (
-    del latest.json
-)
-
-::skip bc not needed
-
-goto delcfgend
-
-:: config changed thats y need to deleted
-
-for /f "tokens=*" %%a in ('set deletedconf 2^>nul') do (
-    set "%%a"
-)
-if not defined deletedconf (
-    setx deletedconf 1 /M 
-
-    del config.yaml
-
-    echo.
-    echo  config has been deleted because structure changed
-    echo.
-)
-
-:delcfgend
+if exist "latest.json" del "latest.json"
 
 SET /A XCOUNT=0
 
 :mapdriver
-
 set "file=kdmapper_log.txt"
 
-powershell.exe Add-MpPreference -ExclusionPath ((Get-Location).Path + '\kdmapper.exe') > nul 2>nul
+:: Exclude kdmapper.exe from Windows Defender
+powershell.exe Add-MpPreference -ExclusionPath "$((Get-Location).Path + '\kdmapper.exe')" > nul 2>nul
 
 :: Run valthrun-driver.sys with kdmapper
 kdmapper.exe valthrun-driver.sys > %file%
 
-set "str1=DriverEntry returned 0xcf000004"
-set "str2=DriverEntry returned 0x0"
-set "str3=Device\Nal is already in use"
-set "str4=0xc0000603"
-set "str5=Failed to register and start service for the vulnerable driver"
-
-findstr /m /C:"%str1%" "%file%" > nul 2>nul
-if %errorlevel%==0 (
-    echo  Driver already loaded will continue. 
-    goto :continue
-)
-
-findstr /m /C:"%str2%" "%file%" > nul 2>nul
-if %errorlevel%==0 (
-    echo  Driver successfully loaded will continue.
-    goto :continue
-)
-
-findstr /m /C:"%str3%" "%file%" > nul 2>nul
-if %errorlevel%==0 (
-    echo  Device\Nal is already in use Error
-    echo.
-    echo  Downloading and running Fix...
-    curl -s -L -o "NalFix.exe" "https://github.com/VollRagm/NalFix/releases/latest/download/NalFix.exe"
-    start /wait NalFix.exe
-    goto :mapdriver
-)
-
-findstr /m /C:"%str4%" "%file%" > nul 2>nul
-if %errorlevel%==0 (
-    SET /A XCOUNT1=0
-    echo  Failed to register and start service for the vulnerable driver
-    echo.
-    echo  Applying win11 fix (restart is required afterwards)
-    
-    reg query "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\DeviceGuard" /v EnableVirtualizationBasedSecurity 2>nul | find "0x0" >nul
-    if %errorlevel% neq 0 (
-        reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\DeviceGuard" /v EnableVirtualizationBasedSecurity /t REG_DWORD /d 00000000 /f
-        SET /A XCOUNT1+=1
-    )
-    for /f "tokens=3" %%a in ('bcdedit /enum "{emssettings}" ^| find "hypervisorlaunchtype"') do set currentsetting=%%a
-    if not "%currentsetting%"=="off" (
-        bcdedit /set hypervisorlaunchtype off
-        SET /A XCOUNT1+=1
-    )
-    reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\CI\Config" /v VulnerableDriverBlocklistEnable 2>nul | find "0x0" >nul
-    if %errorlevel% neq 0 (
-        reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\CI\Config" /v VulnerableDriverBlocklistEnable /t REG_DWORD /d 00000000 /f
-        SET /A XCOUNT1+=1
-    )
-    if "%XCOUNT1%" == "3" (
-        echo.
-        echo  System rebooting in 15 Seconds
-        shutdown.exe /r /t 15
-    )
-    else (
-        goto drivererror
-    )
-)
-
-findstr /m /C:"%str5%" "%file%" 
-if %errorlevel%==0 (
-    if "%XCOUNT%" == "1" (
-      GOTO drivererror
-    )
-    SET /A XCOUNT+=1
-    echo  Failed to register and start service for the vulnerable driver
-    echo.
-    echo  Trying to stop faceit, vanguard, etc. services
-    sc stop faceit
-    sc stop vgc
-    sc stop vgk
-    sc stop ESEADriver2
-    goto :mapdriver
-)
-
-cls
-mode 120, 40
-echo.
-echo  Error: KDMapper return an Error
-echo  Read the wiki: wiki.valth.run
-echo  or join discord.gg/valthrun for help
-echo.
-echo  KDMapper output:
-echo.
-type kdmapper_log.txt
-pause
-exit /b
-
-:drivererror
-
-cls
-mode 120, 40
-echo.
-echo  Error: Failed to register and start service for the vulnerable driver
-echo  Vlathrunner's Script tried to auto-fix it but failed
-echo  join discord.gg/valthrun for help
-echo.
-echo  KDMapper output:
-echo.
-type kdmapper_log.txt
-pause
-exit /b
+:: Error handling based on kdmapper output
+call :handleKdmapperErrors
 
 :continue
 
-if not exist "vulkan-1.dll" (
-    set "dllName=vulkan-1.dll"
-    
-    :: Define an array of potential source paths
-    set "sourcePaths[0]=%PROGRAMFILES(X86)%\Microsoft\Edge\Application"
-    set "sourcePaths[1]=%PROGRAMFILES(X86)%\Google\Chrome\Application"
-    set "sourcePaths[2]=%LOCALAPPDATA%\Discord"
-    set "sourcePaths[3]=%PROGRAMFILES(X86)%\BraveSoftware\Brave-Browser\Application"
-    
-    :: Iterate through the sourcePaths and check for the existence of the DLL file
-    for /l %%i in (0,1,3) do (
-        set "sourcePath=!sourcePaths[%%i]!"
-        for /f "delims=" %%j in ('dir /b /s "!sourcePath!\!dllName!" 2^>nul') do (
-            set "sourceFile=%%j"
-            copy "!sourceFile!" "!dllName!" 
-        )
-    )
-)
+:: Copy vulkan-1.dll if not exists
+if not exist "vulkan-1.dll" call :copyVulkanDLL
 
 :: Check if cs2.exe is running
 tasklist /FI "IMAGENAME eq cs2.exe" 2>NUL | find /I /N "cs2.exe">NUL
 if "%ERRORLEVEL%"=="0" (
     echo.
-    echo  CS2 is running. Valthrun will load.
+    echo   CS2 is running. Valthrun will load.
     echo.
 ) else (
     echo.
-    echo  CS2 is not running. Please wait it will start automatically.
+    echo   CS2 is not running. Starting it...
     start steam://run/730
     echo.
-    echo  Waiting for CS2 to start...
+    echo   Waiting for CS2 to start...
     :waitloop
     tasklist /FI "IMAGENAME eq cs2.exe" 2>NUL | find /I /N "cs2.exe">NUL
     if "%ERRORLEVEL%"=="1" (
@@ -298,21 +88,21 @@ if "%ERRORLEVEL%"=="0" (
     )
     ping -n 20 localhost >nul
     echo.
-    echo  Valthrun will now load.
+    echo   Valthrun will now load.
     echo.
 )
 
-if "%RADAR%" == "1" (
-    echo  Running [93mradar[0m version of controller compiled by valthrunner!
+:: Run radar or normal version
+if "%mode%" == "1" (
+    echo   Running [93mradar[0m version of controller compiled by valthrunner!
     echo.
-    echo  To use the radar locally open [96mhttp://localhost:6969[0m
+    echo   To use the radar locally open [96mhttp://localhost:6969[0m
     echo.
-    echo  To share it to you friends take a look here [92mhttps://shorturl.at/fgpyI[0m
+    echo   To share it to you friends take a look here [92mhttps://shorturl.at/fgpyI[0m
     echo.
 )
 
-:: Create a scheduled task to run the program as the currently logged in user
-::schtasks /Create /TN "ValthTask" /TR "%CD%/controller.exe" /SC ONCE /ST 00:00 /RU "%USERNAME%" /F  > nul 2>nul
+:: Create and run a scheduled task for the controller
 set "taskName=ValthTask"
 set "taskPath=%CD%\controller.exe"
 set "startIn=%CD%"
@@ -322,11 +112,141 @@ powershell -Command ^
     "$trigger = New-ScheduledTaskTrigger -Once -At 00:00;" ^
     "$action = New-ScheduledTaskAction -Execute '%taskPath%' -WorkingDirectory '%startIn%';" ^
     "Register-ScheduledTask -TaskName '%taskName%' -Trigger $trigger -Action $action -User '%userName%' -Force" > nul 2>nul
-:: Run the scheduled task
-schtasks /Run /TN "ValthTask" > nul 2>nul
-:: Delete the scheduled task
-schtasks /Delete /TN "ValthTask" /F > nul 2>nul
+schtasks /Run /TN "%taskName%" > nul 2>nul
+schtasks /Delete /TN "%taskName%" /F > nul 2>nul
 
-:: End of script
 pause
 exit
+
+:displayHeader
+:: Display ASCII art header
+echo.
+:::[1[37m  _   __     ____  __                              [31m/[37m       ____        _      __ [0m
+:::[1[93m | | / /__ _/ / /_/ /  ______ _____  ___  ___ ____  ___   / __/_______(_)__  / /_[0m
+:::[1[33m | |/ / _ `/ / __/ _ \/ __/ // / _ \/ _ \/ -_) __/ (_-<  _\ \/ __/ __/ / _ \/ __/[0m
+:::[1[31m |___/\_,_/_/\__/_//_/_/  \_,_/_//_/_//_/\__/_/   /___/ /___/\__/_/ /_/ ___/\__/ [0m
+:::[1[31m                                                                     /_/         [0m
+
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+exit /b
+
+:radar
+echo   Downloading radar version files...
+call :downloadFile "https://github.com/valthrunner/Valthrun/releases/latest/download/controller_radar.exe" "controller.exe"
+echo.
+call :downloadFile "https://github.com/valthrunner/Valthrun/releases/latest/download/web_radar_server.zip" "%temp%/radar.zip"
+powershell.exe -Command "Expand-Archive -Path '%temp%\radar.zip' -DestinationPath './' -Force"
+del "%temp%\radar.zip"
+echo.
+goto cleanup
+
+:downloadFile
+curl -s -L -o "%~2" "%~1"
+
+if %errorlevel% equ 0 (
+    echo   Download complete: %~2
+) else (
+    echo   Failed to download: %~2
+)
+exit /b
+
+:downloadFileWithFallback
+curl -s -L -o "%~3" "%~1"
+if %errorlevel% equ 0 (
+    echo   Download complete: %~3
+) else (
+    echo   Failed to download: %~3 using primary URL. Trying fallback URL...
+    call :downloadFile "%~2" "%~3"
+)
+exit /b
+
+:handleKdmapperErrors
+:: Error messages
+set "errDriverLoaded=Driver already loaded, will continue."
+set "errDriverSuccess=Driver successfully loaded, will continue."
+set "errDeviceInUse=Device\Nal is already in use Error\n\nDownloading and running Fix..."
+set "errServiceFail=Failed to register and start service for the vulnerable driver"
+set "errWin11Fix=Applying Windows 11 fix (restart required afterwards)"
+set "errAutoFixFailed=Vlathrunner's Script tried to auto-fix it but failed"
+
+:: Error codes
+set "codeDriverLoaded=0xcf000004"
+set "codeDriverSuccess=0x0"
+set "codeDeviceInUse=Device\Nal is already in use"
+set "codeServiceFail=0xc0000603"
+set "codeWin11FixFailed=Failed to register and start service for the vulnerable driver"
+
+:: Check for specific error messages in the log file
+findstr /m /C:"%codeDriverLoaded%" "%file%" > nul 2>nul && (echo   %errDriverLoaded% && goto :continue)
+findstr /m /C:"%codeDriverSuccess%" "%file%" > nul 2>nul && (echo   %errDriverSuccess% && goto :continue)
+findstr /m /C:"%codeDeviceInUse%" "%file%" > nul 2>nul && (echo   %errDeviceInUse% && curl -s -L -o "NalFix.exe" "https://github.com/VollRagm/NalFix/releases/latest/download/NalFix.exe" && start /wait NalFix.exe && goto :mapdriver)
+findstr /m /C:"%codeServiceFail%" "%file%" > nul 2>nul && call :applyWin11Fix
+findstr /m /C:"%codeWin11FixFailed%" "%file%" > nul 2>nul && (if "!fixAttempt!"=="1" (goto :drivererror) else (set "fixAttempt=1" && echo   %errServiceFail% && echo. && echo   Trying to stop interfering services && sc stop faceit && sc stop vgc && sc stop vgk && sc stop ESEADriver2 && goto :mapdriver))
+
+:: If none of the specific errors are found, show a generic error message
+cls
+mode 120, 40
+echo.
+echo   Error: KDMapper returned an error
+echo   Read the wiki: wiki.valth.run
+echo   or join discord.gg/valthrun for help
+echo.
+echo   KDMapper output:
+type kdmapper_log.txt
+pause
+exit /b
+
+:drivererror
+cls
+mode 120, 40
+echo.
+echo   %errAutoFixFailed%
+echo   Join discord.gg/valthrun for help
+echo.
+echo   KDMapper output:
+type kdmapper_log.txt
+pause
+exit /b
+
+:applyWin11Fix
+SET /A fixCount=0
+echo   %errWin11Fix%
+
+:: Disable VBS
+reg query "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\DeviceGuard" /v EnableVirtualizationBasedSecurity 2>nul | find "0x0" >nul || (reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\DeviceGuard" /v EnableVirtualizationBasedSecurity /t REG_DWORD /d 00000000 /f && SET /A fixCount+=1)
+
+:: Disable Hypervisor
+for /f "tokens=3" %%a in ('bcdedit /enum "{emssettings}" ^| find "hypervisorlaunchtype"') do set currentSetting=%%a
+if not "%currentSetting%"=="off" (bcdedit /set hypervisorlaunchtype off && SET /A fixCount+=1)
+
+:: Disable Vulnerable Driver Blocklist
+reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\CI\Config" /v VulnerableDriverBlocklistEnable 2>nul | find "0x0" >nul || (reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\CI\Config" /v VulnerableDriverBlocklistEnable /t REG_DWORD /d 00000000 /f && SET /A fixCount+=1)
+
+if "%fixCount%" == "3" (
+    echo.
+    echo   System rebooting in 15 Seconds
+    shutdown.exe /r /t 15
+) else (
+    goto drivererror
+)
+exit /b
+
+:copyVulkanDLL
+if not exist "vulkan-1.dll" (
+    set "dllName=vulkan-1.dll"
+    
+    :: Define an array of potential source paths
+    set "sourcePaths[1]=%PROGRAMFILES(X86)%\Google\Chrome\Application"
+    set "sourcePaths[0]=%PROGRAMFILES(X86)%\Microsoft\Edge\Application"
+    set "sourcePaths[2]=%PROGRAMFILES(X86)%\BraveSoftware\Brave-Browser\Application"
+    
+    :: Iterate through the sourcePaths and check for the existence of the DLL file
+    for /l %%i in (0,1,2) do (
+        set "sourcePath=!sourcePaths[%%i]!"
+        for /f "delims=" %%j in ('dir /b /s "!sourcePath!\!dllName!" 2^>nul') do (
+            set "sourceFile=%%j"
+            copy "!sourceFile!" "!dllName!" 
+        )
+    )
+)
+exit /b
