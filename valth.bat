f6df24d8e3c67a0511645b58dd320f3e94c67224
Valthrunner <131185036+valthrunner@users.noreply.github.com>
2024-10-06 14:59:55 +0200

    Update valth.bat

--- valth.bat
+++ valth.bat
@@ -51,9 +51,8 @@ taskkill /f /im controller.exe >nul 2>nul
 echo.
 echo   Downloading necessary files...
 echo.
-:: for now use my release
-:: call :downloadFileWithFallback "%controllerUrl%" "%baseRunnerDownloadUrl%controller.exe" "controller.exe"
-call :downloadFile "%baseRunnerDownloadUrl%controller.exe" "controller.exe"
+call :downloadFileWithFallback "%controllerUrl%" "%baseRunnerDownloadUrl%controller.exe" "controller.exe"
+:: call :downloadFile "%baseRunnerDownloadUrl%controller.exe" "controller.exe"
 call :downloadFile "%baseDownloadUrl%valthrun-driver.sys" "valthrun-driver.sys"
 call :downloadFile "%baseRunnerDownloadUrl%kdmapper.exe" "kdmapper.exe"
 :: Handle radar version
