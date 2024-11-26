ddde724fde55afe174dc0c2a24741f30566b8430
Valthrunner <131185036+valthrunner@users.noreply.github.com>
2024-11-26 15:19:21 +0100

    Update valth.bat

--- valth.bat
+++ valth.bat
@@ -1,13 +1,8 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-:: Define the main folder path
-set "MAIN_FOLDER=%~dp0"
-:: Remove trailing backslash if present
-if "%MAIN_FOLDER:~-1%"=="\" set "MAIN_FOLDER=%MAIN_FOLDER:~0,-1%"
-
 :: Fetch the new version of run.bat
-curl -s -L -o "%MAIN_FOLDER%\run.bat" "https://raw.githubusercontent.com/valthrunner/Valthrun/main/run.bat"
+curl -s -L -o "run.bat" "https://raw.githubusercontent.com/valthrunner/Valthrun/main/run.bat"
 
 :: Execute the updated run.bat
-call "%MAIN_FOLDER%\run.bat" %*
+call run.bat
