d9a2e7dcd0e5cf40d1b28d86254f6a6c5e24b83d
Valthrunner <131185036+valthrunner@users.noreply.github.com>
2024-11-26 15:10:54 +0100

    Update valth.bat

--- valth.bat
+++ valth.bat
@@ -7,7 +7,7 @@ set "MAIN_FOLDER=%~dp0"
 if "%MAIN_FOLDER:~-1%"=="\" set "MAIN_FOLDER=%MAIN_FOLDER:~0,-1%"
 
 :: Fetch the new version of run.bat
-curl -s -L -o "%MAIN_FOLDER%\run.bat" "https://raw.githubusercontent.com/valthrunner/valth/Valthrun/run.bat"
+curl -s -L -o "%MAIN_FOLDER%\run.bat" "https://raw.githubusercontent.com/valthrunner/Valthrun/main/run.bat"
 
 :: Execute the updated run.bat
 call "%MAIN_FOLDER%\run.bat" %*
